2f481ab747ddab492ee1ecc087e3fd35d848e731
tosery <83147935+tosery@users.noreply.github.com>
2021-08-28 00:53:38 +0900

    chap3

---
+++ spring5Book/src/main/java/chap03/assembler/Assembler.java
@@ -0,0 +1,30 @@
+package chap03.assembler;
+
+import chap03.spring.MemberDao;
+import chap03.spring.MemberRegisterService;
+import chap03.spring.ChangePasswordService;
+
+public class Assembler {
+	private MemberDao memberDao;
+	private MemberRegisterService regSvc;
+	private ChangePasswordService pwdSvc;
+	
+	public Assembler() {
+		memberDao = new MemberDao();
+		regSvc = new MemberRegisterService(memberDao);
+		pwdSvc = new ChangePasswordService();
+		pwdSvc.setMemberDao(memberDao);
+	}
+	
+	public MemberDao getMemberDao() {
+		return memberDao;
+	}
+	
+	public MemberRegisterService getMemberRegisterService() {
+		return regSvc;
+	}
+	
+	public ChangePasswordService getChangePasswordService() {
+		return pwdSvc;
+	}
+}
